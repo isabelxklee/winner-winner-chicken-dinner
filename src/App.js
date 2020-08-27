1daa5bb8e7b13c9c4765874b363f46c12abc4bbe
isabel k. lee <hello@isabelklee.co>
2020-08-26 22:15:56 -0400

    cleaned up default files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './App.css'
+import Home from './components/Home'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    )
+  }
+}
+
+export default App
