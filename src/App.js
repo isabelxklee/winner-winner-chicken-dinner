e3f9cdea5e255be27a0bc026f2907dd1f7b0380f
isabel k. lee <hello@isabelklee.co>
2020-09-02 16:39:17 -0400

    added fonts and cleaned up styling riles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import Home from './components/Home'
-import './App.css'
 
 class App extends Component {
   render() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,12 @@ import styled from 'styled-components'
 import CreateListForm from './CreateListForm'
 
 const Wrapper = styled.div`
-  padding: 4em;
-  background: black;
+  padding: 9em;
+  background: radial-gradient(#E3E73B, #FFF 65%);
+
+  @media(max-width: 720px) {
+    padding: 4em;
+  }
 `
 
 class Home extends Component {
