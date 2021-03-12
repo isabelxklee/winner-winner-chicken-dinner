8a7a460e1d5f106dfccc9bb57fe9496ba233f227
isabelxklee <hello@isabelklee.co>
2021-03-12 13:05:36 -0500

    installed prettier

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Wrapper} from '../../styles'
+import LinkButton from '../styled-components/LinkButton.jsx'
+
+const Home = () => (
+  <Wrapper primary>
+    <h1>Winner winner, chicken dinner!</h1>
+    <p>Enter a list and spin the wheel to get a random winner each time. Ready to get started?</p>
+
+    <div className="section">
+      <LinkButton href="/create-list" primary>
+        Create a list
+      </LinkButton>
+      {localStorage.length > 0 ? (
+        <LinkButton href="/list" primary>
+          See my existing list
+        </LinkButton>
+      ) : (
+        <></>
+      )}
+    </div>
+  </Wrapper>
+)
+export default Home
